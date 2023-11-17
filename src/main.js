188912ec76f047d421c5f83b3ea11bb3ccb733f8
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-11-17 09:14:23 +0100

    feat: new topics added

--- src/main.js
+++ src/main.js
@@ -40,3 +40,80 @@ let x;
 x = ["hello", 10]; // OK
 // Initialize it incorrectly
 //x = [10, "hello"]; // Error
+console.log(x[0].substring(1)); // OK
+//console.log(x[1].substr(1)); // Error, 'number' doesnot have 'substr'
+console.log(`x[0]: ${x[0]}`);
+console.log(`x[1]: ${x[1]}`);
+//console.log(`x[2]: ${x[2]}`); erro
+let y1;
+let y2;
+[y1, y2] = x;
+console.log(`y1: ${y1}`);
+console.log(`y2: ${y2}`);
+// Optional tuples
+let optionalTuple;
+optionalTuple = ["test2", true];
+console.log(`optionalTuple: ${optionalTuple}`);
+optionalTuple = ["test"];
+console.log(`optionalTuple: ${optionalTuple}`);
+// Enums
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Green;
+console.log(c); // This will output '1'
+console.log(Color[c]); // This will output 'Green'
+// String Enums
+var DoorStateString;
+(function (DoorStateString) {
+    DoorStateString["Open"] = "open";
+    DoorStateString["Closed"] = "closed";
+    DoorStateString["Ajar"] = "ajar";
+})(DoorStateString || (DoorStateString = {}));
+var openDoorString = DoorStateString.Open;
+console.log(`openDoorString = ${openDoorString}`);
+// Any
+let notSure = 4;
+notSure = " maybe a string instead";
+notSure = false; // ok, this is definitely boolean
+let looselyTyped = 4;
+console.log(`looselyTyped is of type: ${typeof looselyTyped}`);
+// Void
+function warnUser() {
+    alert("This is my warning message");
+}
+// warnUser();
+// Null and Undefined
+let n = null; // Primitive types can be null
+let m = undefined; // Primitive types can beundefined
+let x_ = null; // same as x: any = null
+let y = undefined; // same as y: any = undefined
+// x_ =1; error
+// y = 'hello'; error
+// Never
+function error(message) {
+    throw new Error(message);
+}
+//error("Error");
+// Symbol
+// Type assertion
+let someValue = "this is a string";
+// down here we are telling typescript that someValue is a string and not any other type to perform a conversion
+let strLength = someValue.length;
+console.log(`strLength: ${typeof strLength}`);
+let someValue2 = "this is a string";
+let strLength2 = someValue.length;
+function f() {
+    var a = 1;
+    a = 2;
+    var b = g();
+    a = 3;
+    return b;
+    function g() {
+        return a;
+    }
+}
+console.log(f());
--- src/main.ts
+++ src/main.ts
@@ -48,3 +48,95 @@ let x: [string, number];
 x = ["hello", 10]; // OK
 // Initialize it incorrectly
 //x = [10, "hello"]; // Error
+console.log(x[0].substring(1)); // OK
+//console.log(x[1].substr(1)); // Error, 'number' doesnot have 'substr'
+console.log(`x[0]: ${x[0]}`);
+console.log(`x[1]: ${x[1]}`);
+//console.log(`x[2]: ${x[2]}`); erro
+
+let y1: string;
+let y2: number;
+[y1, y2] = x;
+
+console.log(`y1: ${y1}`);
+console.log(`y2: ${y2}`);
+
+// Optional tuples
+let optionalTuple: [string, boolean?];
+optionalTuple = ["test2", true];
+console.log(`optionalTuple: ${optionalTuple}`);
+optionalTuple = ["test"];
+console.log(`optionalTuple: ${optionalTuple}`);
+
+// Enums
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+let c: Color = Color.Green;
+console.log(c); // This will output '1'
+console.log(Color[c]); // This will output 'Green'
+
+// String Enums
+
+enum DoorStateString {
+  Open = "open",
+  Closed = "closed",
+  Ajar = "ajar",
+}
+var openDoorString = DoorStateString.Open;
+console.log(`openDoorString = ${openDoorString}`);
+
+// Any
+
+let notSure: any = 4;
+notSure = " maybe a string instead";
+notSure = false; // ok, this is definitely boolean
+
+let looselyTyped: any = 4;
+
+console.log(`looselyTyped is of type: ${typeof looselyTyped}`);
+
+// Void
+function warnUser(): void {
+  alert("This is my warning message");
+}
+// warnUser();
+
+// Null and Undefined
+let n: number | null = null; // Primitive types can be null
+let m: number | undefined = undefined; // Primitive types can beundefined
+let x_: null = null; // same as x: any = null
+let y: undefined = undefined; // same as y: any = undefined
+// x_ =1; error
+// y = 'hello'; error
+
+// Never
+function error(message: string): never {
+  throw new Error(message);
+}
+//error("Error");
+
+// Symbol
+
+// Type assertion
+let someValue: any = "this is a string";
+// down here we are telling typescript that someValue is a string and not any other type to perform a conversion
+let strLength: number = (<string>someValue).length;
+console.log(`strLength: ${typeof strLength}`);
+
+let someValue2: any = "this is a string";
+let strLength2: number = (someValue as string).length;
+
+function f() {
+  var a = 1;
+  a = 2;
+  var b = g();
+  a = 3;
+  return b;
+  function g() {
+    return a;
+  }
+}
+console.log(f());

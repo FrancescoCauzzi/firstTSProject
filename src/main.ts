a9de222f7d0ba6fb3387d8669634e3c4cc2aa1c8
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-11-22 20:14:33 +0100

    feat: module and namespaces added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-// MODULES AMD NAMESPACES
+interface IModel {
+  DisplayName: string;
+  Id: number;
+}
+class Model implements IModel {
+  DisplayName: string;
+  Id: number;
+  constructor(model: IModel) {
+    this.DisplayName = model.DisplayName;
+    this.Id = model.Id;
+  }
+}
+let firstModel = new Model({ Id: 1, DisplayName: "firstModel" });
 
-import { T2 } from "./declaration";
-import * as declarations from "./declaration";
-console.log(declarations.v1);
-
-import { Bird } from "./declaration";
-const birdAll = [Bird.Raven, Bird.Owl];
-
-// Reexport
+console.log(firstModel.DisplayName);
---
+++ src/main6_modules_and_namespaces.ts
@@ -0,0 +1,11 @@
+// MODULES AMD NAMESPACES
+
+import { T2 } from "./declaration";
+import * as declarations from "./declaration";
+console.log(declarations.v1);
+
+import { Bird } from "./declaration";
+const birdAll = [Bird.Raven, Bird.Owl];
+
+// Reexport
+console.log("Hello there");
